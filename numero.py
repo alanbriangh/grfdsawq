1664eb7ed0ea237ec79e8845484157ce744930e8
Alan Brian <36174194+alanbriangh@users.noreply.github.com>
2023-01-10 15:27:17 -0300

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,17 @@
+import base64
+import zlib
+
+def compress_and_encode(data):
+    compressed_data = zlib.compress(data)
+    encoded_data = base64.b64encode(compressed_data)
+    return encoded_data
+
+def decode_and_decompress(encoded_data):
+    compressed_data = base64.b64decode(encoded_data)
+    data = zlib.decompress(compressed_data)
+    return data
+
+encoded_data = "eJwzNjECAAE2AJo="
+data = decode_and_decompress(encoded_data)
+
+print(data)
